f87c78de4497647af34babd64ad7fc4346f50bae
catom <634358219@qq.com>
2016-04-06 19:29:36 +0800

    获取代码

--- DB/LM_dev.sql
+++ DB/LM_dev.sql
@@ -7,16 +7,16 @@
 use [Test] 
 go
 
--- 取上一条写入的数据的Identity类型的字段值
+-- 1. 取上一条写入的数据的Identity类型的字段值
 select @@identity as [LastID]
 
 
-select * from dbo.[User]  
-
-insert into dbo.[User](Name,Age)
-values('a6',6)
-go
-
+-- 2. 分页查询
+--	pageIndex = 3,pageSize = 8
+select top 8 * from (
+	select top 32 row_number() over(order by id) as RowNumber,* from Novel
+)A
+where A.rownumber > 3*8
 
 
 
